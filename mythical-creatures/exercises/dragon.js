ed9020a0bff2e5de4b303fb86ada4ec30b42d4b1
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-08-30 07:57:05 -0600

    Complete Dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,15 +1,17 @@
 class Dragon {
-  constructor(name, rider) {
+  constructor(name, rider, hungry = true) {
     this.name = name;
     this.rider = rider;
-    this.hungry = true;
+    this.hungry = hungry;
+    this.hungryCount = 0;
   }
   greet() {
     return `Hi, ${this.rider}!`
   }
-  eat() {
-    
-  }
+  eat(hungry) {
+    this.hungryCount++
+    if (this.hungryCount >= 3) {return this.hungry = false}
+}
 }
 
 
