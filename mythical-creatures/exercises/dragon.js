318fb9629fa9c3e16cec3297525be7099eb9eaaf
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-08-29 19:04:52 -0600

    Work trough the first 6 tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,16 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+  eat() {
+    
+  }
+}
 
+
+module.exports = Dragon;
